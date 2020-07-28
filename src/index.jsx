ba2c924512805dc7cacf349525e705f5477960c9
Yuliya Haluza <c-yuliya.haluza@hulu.com>
2020-07-28 17:02:27 +0300

    feat: add media

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import { hot } from 'react-hot-loader';
+
+import image1 from './assets/images/image1.jpg';
+import image2 from './assets/images/image2.png';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { active: false };
+  }
+
+  toggleActive = () => {
+    this.setState(prevState => ({ active: !prevState.active }));
+  };
+
   render() {
-    return <div>Hello {this.props.name}</div>;
+    const { name } = this.props;
+    const { active } = this.state;
+    return (
+      <>
+        <button type="button" onClick={this.toggleActive}>
+          Hello {name}! Press to toggle image
+        </button>
+        <div>
+          <img  width="300" src={active ? image1 : image2} alt="cat" />
+        </div>
+      </>
+    );
   }
 }
 
+App.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
 const AppWithHot = hot(module)(App);
 
-var mountNode = document.getElementById('app');
+const mountNode = document.getElementById('app');
 ReactDOM.render(<AppWithHot name="Jane" />, mountNode);
